1c1ea699fe7bd36c9878410a261df54165213855
Wes Alvaro <wesalvaro@google.com>
2013-08-19 16:25:22 -0700

    Sets default path to dotzsh module path

--- init.zsh
+++ init.zsh
@@ -12,7 +12,7 @@ autoload -Uz add-zsh-hook
 
 # Tallies your usage of aliases and functions.
 function tally-aliaster {
-  python /path/to/aliaster.py \
+  python $DOTZSH/modules/aliaster/aliaster.py \
     $1 "`whence -w $1 | egrep -q \"function|alias\" && whence -f $1 | wc -c`"
   export ALIASTER=$?
 }
